7602217260877859a6293eccec88d8e1e7e7da89
branliang <bran.liang@shopify.com>
2023-02-04 12:17:09 +0700

    Add basic wrapper

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # iproute
+
+## Getting start
+
+Only works for macos as it invokes `route` command internally.
+
+```
+iproute <ARGS SAME AS route>
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process::Command;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+
+    let output = Command::new("route")
+            .args(args)
+            .output()
+            .expect("failed to run route command");
+
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
 }
